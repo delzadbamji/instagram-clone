08ce7bb9fe3af45e168fcf3d1872c7af76ee0f26
Delzad <delzadbamji@gmail.com>
2021-05-19 01:45:33 -0400

    update login

--- src/pages/login.js
+++ src/pages/login.js
@@ -20,16 +20,26 @@ export default function Login() {
 				<img src="/images/iphone-with-profile.jpg" alt="iphone with insta" />
 			</div>
 			<div className="flex flex-col w-2/5">
-			<h1 className="flex justify-center w-full">
-				<img src="/images/logo.png" alt="Logo" className="mt-2 w-6/12 mb-4"></img>
+				<h1 className="flex justify-center w-full">
+					<img src="/images/logo.png" alt="Logo" className="mt-2 w-6/12 mb-4" />
 				</h1>
-	{error && <p className="mb-4 text-xs text-red-primary">{error}</p>}	
-	<form onSubmit={handleLogin} method="POST">
-			<input aria-label="enter your email"
-			type="text"
-			placeholder="Email Address"
-			className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2"/>
-			</form>
+				{error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+				<form onSubmit={handleLogin} method="POST">
+					<input
+						aria-label="enter your email"
+						type="text"
+						placeholder="Email Address"
+						className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+						onChange={({ target }) => setEmail(target.value)}
+					/>
+					<input
+						aria-label="enter your email"
+						type="password"
+						placeholder="Password"
+						className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+						onChange={({ target }) => setPassword(target.value)}
+					/>
+				</form>
 			</div>
 		</div>
 	);
