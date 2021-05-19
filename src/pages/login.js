59040eb42ba4b0dfd1eb09f677202a907d10ff90
Delzad <delzadbamji@gmail.com>
2021-05-19 01:03:51 -0400

    login email

--- src/pages/login.js
+++ src/pages/login.js
@@ -20,7 +20,16 @@ export default function Login() {
 				<img src="/images/iphone-with-profile.jpg" alt="iphone with insta" />
 			</div>
 			<div className="flex flex-col w-2/5">
-				<p>Form!</p>
+			<h1 className="flex justify-center w-full">
+				<img src="/images/logo.png" alt="Logo" className="mt-2 w-6/12 mb-4"></img>
+				</h1>
+	{error && <p className="mb-4 text-xs text-red-primary">{error}</p>}	
+	<form onSubmit={handleLogin} method="POST">
+			<input aria-label="enter your email"
+			type="text"
+			placeholder="Email Address"
+			className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2"/>
+			</form>
 			</div>
 		</div>
 	);
