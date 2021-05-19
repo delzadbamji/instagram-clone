dddd55b8231a18483a6dcd8ae60c6782ca654577
Delzad <delzadbamji@gmail.com>
2021-05-19 00:13:24 -0400

    postcss tailwindcss config

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import FirebaseContext from './context/firebase';
 import App from './App';
 import { firebase, FieldValue } from './lib/firebase';
-
+import './styles/app.css';
 ReactDOM.render(
 	<FirebaseContext.Provider value={{ firebase, FieldValue }}>
 		<App />
